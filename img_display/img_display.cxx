0d263a7ea69425c6a9c4eaa9c38eec14c813d4bb
Fan Yang <stoneyang0915@gmail.com>
2015-01-06 10:58:04 +0800

    ENH: added in simple flips

--- img_display/img_display.cxx
+++ img_display/img_display.cxx
@@ -6,19 +6,30 @@
 int main( int argc, char* argv[] )
 {
     // read an image
-    cv::Mat image = cv::imread( "lena.jpg" );
+    cv::Mat src = cv::imread( "lena.jpg" );
     // create image window named "lena"
     cv::namedWindow( "lena" );
     // show the image on window
-    cv::imshow( "lena", image );
+    cv::imshow( "lena", src );
     // show the size of the image
     std::cout << "The size of lena: " << std::endl;
-    std::cout << image.size().height << std::endl;
-    std::cout << image.size().width << std::endl;
+    std::cout << src.size().height << std::endl;
+    std::cout << src.size().width << std::endl;
+    // flip the image...
+    cv::Mat dst, dst2;
+
+    cv::flip( src, dst, 1 );
+    cv::namedWindow( "lena_flip" );
+    cv::imshow( "lena_flip", dst );
+
+    cv::flip( src, dst2, 0 );
+    cv::namedWindow( "lena_flip2" );
+    cv::imshow( "lena_flip2", dst2 );
     // wait key for 5000ms
     //cv::waitKey( 5000 );
     cv::waitKey( 0 );
-    cv::imwrite( "lena_output.jpg", image );
+    cv::imwrite( "lena_output.jpg", dst );
+    cv::imwrite( "lena_output2.jpg", dst2 );
 
     return 0;
 }
